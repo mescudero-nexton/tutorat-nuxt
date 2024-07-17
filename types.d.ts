d2049569cc60c4dbcf10c8e8aec75856cfbe5170
Martial ESCUDERO <135983261+mescudero-nexton@users.noreply.github.com>
2024-07-17 09:31:02 +0200

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,1 @@
-interface User {
-  id: number;
-  username: string;
-  email: string;
-  provider: string;
-  confirmed: boolean;
-  blocked: boolean;
-  created_at: string;
-  updated_at: string;
-}
+//
